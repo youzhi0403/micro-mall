1a19b87ed9652914ba71f46812040c8869123a5a
cwj <15521384566@163.com>
2019-12-05 09:55:27 +0800

    初始化项目

---
+++ micro-mall-admin/src/main/java/com/youzhi/MircoMallAdminApplication.java
@@ -0,0 +1,13 @@
+package com.youzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MircoMallAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MircoMallAdminApplication.class, args);
+    }
+
+}
---
+++ micro-mall-admin/src/test/java/com/youzhi/MircoMallAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youzhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MircoMallAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

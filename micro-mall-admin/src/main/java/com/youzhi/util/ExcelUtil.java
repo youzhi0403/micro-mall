4fda35d6f15f04b6d0fc9d642427591b8e1e0fd1
cwj <15521384566@163.com>
2020-01-20 08:46:57 +0800

    提交excel工具类

--- micro-mall-admin/src/main/java/com/youzhi/util/ExcelUtil.java
+++ micro-mall-admin/src/main/java/com/youzhi/util/ExcelUtil.java
@@ -1,35 +1,39 @@
 package com.youzhi.util;
+
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.net.URLEncoder;
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import org.apache.poi.ss.usermodel.CellType;
 
 /**
- * @description: 基于POI的Excel工具类
- * @author: cwj
+ * @author cwj
+ * @date 2020/1/20 8:32
  * 出自：https://blog.csdn.net/weixin_42043030/article/details/99686731
- **/
+ * 基于POI的excel工具类
+ */
 public class ExcelUtil {
-
-    private static final String XML ="xls";
+    private static final String XLS ="xls";
     private static final String XLSX ="xlsx";
 
     /**
-     * @description: // todo 读取上传的Excel文件
-     * @author: xiao.wang@newtouch.com
-     * @param file 上传的文件
-     * @param startRow  开始行 0开始
-     * @param startCell 开始列 0开始
-     * */
+     * 读取上传的excel文件
+     * @param file
+     * @param startRow
+     * @param startCell
+     * @return
+     * @throws Exception
+     */
     public static List<String[]> readExcel(MultipartFile file, int startRow, int startCell)throws Exception{
         checkFile(file);
         String fileName = file.getOriginalFilename();
@@ -62,74 +66,30 @@ public class ExcelUtil {
         return list;
     }
 
-
-    /***
-     * //todo 校验文件是否正常
-     * @param file 上传的文件
+    /**
+     * 校验文件是否正常
+     * @param file
      * @throws Exception
      */
     private static void checkFile(MultipartFile file)throws Exception{
         if (Objects.isNull(file))
             throw new FileNotFoundException("上传文件是空");
         String fileName = file.getOriginalFilename();
-        if(!fileName.endsWith(XML) && !fileName.endsWith(XLSX))
+        if(!fileName.endsWith(XLS) && !fileName.endsWith(XLSX))
             throw new IOException(fileName + "不是excel文件");
     }
 
     /**
-     * @description: // todo 获取单元格的值
-     * @author: xiao.wang@newtouch.com
-     * @param cell 单元格
-     * @return String
+     * 获取上传文件的工作区
+     * @param file
+     * @param fileName
+     * @return
      */
-    private static String getCellValue(Cell cell){
-        String cellValue = "";
-        if(cell == null){
-            return cellValue;
-        }
-        //把数字当成String来读，避免出现1读成1.0的情况
-        if(cell.getCellType() == Cell.CELL_TYPE_NUMERIC:value){
-            cell.setCellType(Cell.CELL_TYPE_STRING);
-        }
-        //判断数据的类型
-        switch (cell.getCellType()){
-            case Cell.CELL_TYPE_NUMERIC: //数字
-                cellValue = String.valueOf(cell.getNumericCellValue());
-                break;
-            case Cell.CELL_TYPE_STRING: //字符串
-                cellValue = String.valueOf(cell.getStringCellValue());
-                break;
-            case Cell.CELL_TYPE_BOOLEAN: //Boolean
-                cellValue = String.valueOf(cell.getBooleanCellValue());
-                break;
-            case Cell.CELL_TYPE_FORMULA: //公式
-                cellValue = String.valueOf(cell.getCellFormula());
-                break;
-            case Cell.CELL_TYPE_BLANK: //空值
-                cellValue = "";
-                break;
-            case Cell.CELL_TYPE_ERROR: //故障
-                cellValue = "非法字符";
-                break;
-            default:
-                cellValue = "未知类型";
-                break;
-        }
-        return cellValue;
-    }
-
-    /**
-     * @author: xiao.wang@newtouch.com
-     * @description: //todo 获取上传文件的工作区
-     * @param file 上传的文件
-     * @param fileName 文件名字
-     * @return Workbook
-     */
-    private static Workbook getWorkBook(MultipartFile file,String fileName) {
+    private static Workbook getWorkBook(MultipartFile file, String fileName) {
         Workbook workbook = null;
         try {
             InputStream is = file.getInputStream();
-            if(fileName.endsWith(XML)){
+            if(fileName.endsWith(XLS)){
                 //2003
                 workbook = new HSSFWorkbook(is);
             }else if(fileName.endsWith(XLSX)){
@@ -143,25 +103,45 @@ public class ExcelUtil {
     }
 
     /**
-     * @author: xiao.wang@newtouch.com
-     * @description: //todo 获取上传文件的工作区
-     * @param file 读取到的文件
-     * @return Workbook
+     * 获取单元格的值
+     * @param cell
+     * @return
      */
-    private static Workbook getWorkBook(File file) {
-        Workbook workbook = null;
-        try {
-            InputStream is = new FileInputStream(file);
-            if(file.getName().endsWith(XML)){
-                //2003
-                workbook = new HSSFWorkbook(is);
-            }else if(file.getName().endsWith(XLSX)){
-                //2007
-                workbook = new XSSFWorkbook(is);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static String getCellValue(Cell cell){
+        String cellValue = "";
+        if(cell == null){
+            return cellValue;
         }
-        return workbook;
+        //把数字当成String来读，避免出现1读成1.0的情况
+        if(cell.getCellType() == CellType.NUMERIC){
+            cell.setCellType(CellType.STRING);
+        }
+        //判断数据的类型
+        switch (cell.getCellType()){
+            case NUMERIC: //数字
+                cellValue = String.valueOf(cell.getNumericCellValue());
+                break;
+            case STRING: //字符串
+                cellValue = String.valueOf(cell.getStringCellValue());
+                break;
+            case BOOLEAN: //Boolean
+                cellValue = String.valueOf(cell.getBooleanCellValue());
+                break;
+            case FORMULA: //公式
+                cellValue = String.valueOf(cell.getCellFormula());
+                break;
+            case BLANK: //空值
+                cellValue = "";
+                break;
+            case ERROR: //故障
+                cellValue = "非法字符";
+                break;
+            default:
+                cellValue = "未知类型";
+                break;
+        }
+        return cellValue;
     }
+
+
 }
